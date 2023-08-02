8a6d7b0f7c52f9a14530462cbf5f912e2cbd05df
Mohit <mohit.sharma0690@gmail.com>
2023-08-01 20:30:01 -0400

    Try to fix videos not loading.

--- static/js/index.js
+++ static/js/index.js
@@ -65,13 +65,14 @@ $(document).ready(function() {
         player.currentTime = player.duration / 100 * this.value;
       })
     }, false);*/
-    preloadInterpolationImages();
 
-    $('#interpolation-slider').on('input', function(event) {
-      setInterpolationImage(this.value);
-    });
-    setInterpolationImage(0);
-    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
+    // preloadInterpolationImages();
+
+    // $('#interpolation-slider').on('input', function(event) {
+    //   setInterpolationImage(this.value);
+    // });
+    // setInterpolationImage(0);
+    // $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
 
     bulmaSlider.attach();
 
